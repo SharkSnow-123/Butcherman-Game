55b77d8b3c0948ad8a7873984339bec2748e2907
lauraescasinas <lauramargaret.escasinas@cit.edu>
2025-12-02 23:10:03 +0800

    lost and won scenes

--- death_scene.gd
+++ death_scene.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
 
-
-func _on_end_button_pressed():
+func _on_losttomain_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
--- end_scene_1.gd
+++ end_scene_1.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
-func _on_end_button_pressed():
+
+func _on_wontomain_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
