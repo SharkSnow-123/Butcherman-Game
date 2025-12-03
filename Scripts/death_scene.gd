61f3a67fc361abea03b1eafb0f8b03a0640c02f2
SharkSnow-123 <briarrovic.elemino@cit.edu>
2025-12-03 19:50:29 +0800

    Put transition to the global, reworked ending buttons to the root script

--- Scripts/death_scene.gd
+++ Scripts/death_scene.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 
 
-func _on_end_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+func _on_losttomain_pressed() -> void:
+	pass # Replace with function body.
