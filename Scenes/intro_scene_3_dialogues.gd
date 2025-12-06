e1cf6e92fdeaf1d48c22e3b16608c11e53d568e5
lauraescasinas <lauramargaret.escasinas@cit.edu>
2025-12-06 16:30:48 +0800

    Revert "try"
    This reverts commit 106aa1c113f2d8e0ea9d70a5dde178928dc180b5.

--- Scenes/intro_scene_3_dialogues.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var this_panel = $Panel
-@onready var this_label = $Panel/StoryText
-
-
-func _ready():
-	TransitionScreen.fade_in()
---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/IntroScene1.tscn")
+	
+func _on_texture_button_pressed() -> void:
+	MusicPlayer.set_playing(!MusicPlayer.is_playing())
+
+func _ready() -> void:
+	MusicPlayer.play_music("res://Music/Menu Music.wav")
