90a8febe871dda8b5b2641b17fabcea2306da9ec
mika-dylan <mikadylan.bartido@cit.edu>
2025-12-10 09:26:36 +0800

    fixed: main script

--- Scripts/game_play_scene.gd
+++ Scripts/game_play_scene.gd
@@ -1,16 +1,21 @@
 extends Node
 const RecodeLowLevel = preload("res://Scripts/stack_low_level.gd")
 var ArrayLowLevel = preload("res://Scripts/array_low_level.gd").new()
+
 #--- Notes ---
 # Hello, greetings to whoever is reading this. This is merely a prototype and there's a lot of things to implement to. 
 # UNDO is still built-in, not manually coded to see how the game works. So, as arrays.
 # Note written by: Sharksnow-123 (Briar)
 
+#when writing constants, be sure to CAPITALIZE them! --SHARKIE (OwO /)
+
+
+
 # --- SETTINGS ---
 const MAX_GUESSES := 5
-var word_list_day1 = ["APPLE", "ROBOT", "SNAKE"]
-var word_list_day2 = ["WATERFALL", "NOTEBOOK", "PYTHON"]
-var word_list_day3 = ["ASTRONOMY", "COMPUTER", "VOLCANO"]
+#var word_list_day1 = ["APPLE", "ROBOT", "SNAKE"]
+#var word_list_day2 = ["WATERFALL", "NOTEBOOK", "PYTHON"]
+#var word_list_day3 = ["ASTRONOMY", "COMPUTER", "VOLCANO"]
 
 # --- LOW LEVEL SETTINGS ---
 #const MAX_WORD_SIZE := 32
@@ -24,11 +29,14 @@ var redo_stack : RecodeLowLevel          # ^^
 var undoCtr = 0;
 var redoCtr = 0;
 
+var word_answer = [
+	"LIGHT",
+	"CANDLE",
+	"KEY"
+]
 
 # --- STATE ---
 var chosen_word := ""
-#var hidden := []
-#var guessed_letters := []
 var wrong_guesses := 0
 var current_day := 1
 const MAX_DAYS := 3
@@ -48,17 +56,61 @@ var currentHealth: int = MAX_GUESSES
 @onready var return_button = $LosePanel/ReturnMain
 @onready var day_frame = $DayFrame
 
+
+@onready var clue_button = $ClueButton
+@onready var this_panel = $Dialogue_Panel
+@onready var this_label = $Dialogue_Panel/StoryText
+
+
+# ----------------------------------
+# DIALOGUES
+# ----------------------------------
+
+var scene_dialogues = [
+	#DAY 1
+	["Ah… it’s been a while. You’re still my friend, right?", 
+	"I'm thankful you were my first.", "I know you can’t talk right now… but you understand.",
+	"Let’s play a game. It's called, Butcherman.", "See the letters? One mistake, and you feel pain. Simple.", 
+	"Here’s your riddle: I can fill a room but take up no space. What am I?"
+	],
+	#DAY 2
+	["How are you feeling?", "Still surprised you’ve got all your limbs? That’s thanks to me. I wouldn’t leave my friend hanging.",
+	"You know how this works by now. You did well yesterday.", "So let’s make things harder.", 
+	"Riddle time: I burn to give you light, but the more I shine, the shorter I get. A single breath can end me. What am I?"
+	],
+	#DAY 3
+	[ "Three days and you’re still alive. Lost a bit of blood here and there, but you’re breathing—good job.", 
+	"Here’s the deal: answer this one, and I’ll let you go",
+	"How many times do I have to say it? The more you fight, the worse it gets.",
+	"Anyway, here’s your final riddle.",
+	"If I turn once, what’s outside won’t get in. Turn again, what’s inside won’t get out. What am I?"
+	]
+	
+]
+
+var riddles = [
+	#Day 1
+	["Here’s your riddle: I can fill a room but take up no space. What am I?"],
+	
+	#Day 2
+	["I burn to give you light, but the more I shine, the shorter I get. A single breath can end me. What am I?"],
+	
+	#Day3
+	["If I turn once, what’s outside won’t get in. Turn again, what’s inside won’t get out. What am I?"]
+]
+
+
 func _ready():
-	#hidden.resize(MAX_WORD_SIZE)
-	#guessed_letters.resize(MAX_GUESSED)
 	ArrayLowLevel.hidden.resize(ArrayLowLevel.MAX_WORD_SIZE)
 	ArrayLowLevel.guessed_letters.resize(ArrayLowLevel.MAX_GUESSED)
-	
 	undo_stack = RecodeLowLevel.new(50)
 	redo_stack = RecodeLowLevel.new(50)
+	
 	randomize()
 	connect_buttons()
 	start_game()
+	TransitionScreen.fade_in()
+	MusicPlayer.play_music("res://Music/gameplay_scene_bgm.mp3")
 
 
 # ----------------------------------
@@ -69,7 +121,6 @@ func start_game():
 	wrong_guesses = 0
 	undoCtr = 0
 	redoCtr = 0
-	#guessed_letters.clear()
 	ArrayLowLevel.clear_guessed()
 	ArrayLowLevel.clear_hidden()
 	undo_stack.clear()
@@ -77,11 +128,10 @@ func start_game():
 
 	# choose a word based on current day
 	chosen_word = _get_word_for_day()
-	#hidden.clear()
-	
+	play_Day_Dialogue()
+
 	for c in chosen_word:
 		ArrayLowLevel.hidden_push("_")
-		#hidden.append("_")
 
 	update_ui()
 	update_health_display()
@@ -93,39 +143,42 @@ func start_game():
 
 
 func _get_word_for_day() -> String:
-	# pick a random word according to current day
-	if current_day == 1:
-		return word_list_day1[randi() % word_list_day1.size()]
-	elif current_day == 2:
-		return word_list_day2[randi() % word_list_day2.size()]
-	else:
-		return word_list_day3[randi() % word_list_day3.size()]
-
+	var index = current_day - 1
+	
+	if index >= word_answer.size():
+		index = word_answer.size()-1
+	
+	return word_answer[index]
 
 # ----------------------------------
 # CONNECT KEY BUTTONS
 # ----------------------------------
+func button_behavior(b:String):
+	handle_letter(b)
+	$AudioStreamPlayer.play()
+
+
 func connect_buttons():
 	# connect letter buttons safely (capture local reference)
 	for btn in letters.get_children():
 		if btn is Button:
 			var b = btn
-			b.pressed.connect(func(): handle_letter(b.text))
+			b.pressed.connect(self.button_behavior.bind(b.text))
 
 	# other buttons
 	undo_button.pressed.connect(undo)
 	redo_button.pressed.connect(redo)
 	continue_button.pressed.connect(_on_continue_pressed)
 	return_button.pressed.connect(_on_return_main_pressed)
+	clue_button.pressed.connect(_on_clue_button_pressed)
+	
 
 
 # ----------------------------------
 # UPDATE DISPLAY
 # ----------------------------------
 func update_ui():
-	#word_label.text = " ".join(hidden)
 	word_label.text = ArrayLowLevel.build_hidden()
-	#guessed_label.text = "Guessed: " + ", ".join(guessed_letters)
 	guessed_label.text = "Guessed: " + ArrayLowLevel.build_guessed()
 
 
@@ -158,21 +211,19 @@ func update_health_display():
 		print("Heart State: ", hearts[currentHealth - 1])
 		hearts[currentHealth - 1].visible = true;
 		
-
+	
+	
 # ----------------------------------
 # GUESS LETTER
 # ----------------------------------
 func handle_letter(letter):
 	letter = letter.to_upper()
 
-	#if letter in guessed_letters:
-		#return
 	if ArrayLowLevel.guessed_checker(letter):
 		return
 
 	save_state()
 
-	#guessed_letters.append(letter)
 	ArrayLowLevel.guessed_push(letter)
 	update_ui()
 
@@ -184,26 +235,26 @@ func handle_letter(letter):
 			correct = true
 
 	update_ui()
-	
 	var _still_blank := false
 	for i in range(ArrayLowLevel.hidden_size):
 		if ArrayLowLevel.hidden[i] == "_":
 			_still_blank = true
 			break
-	
+		
 	if correct:
 		if not _still_blank:
 			show_end("          YOU WIN!\n Don't let it get to your head.")
-		return
-	# else:
-	wrong_guesses += 1
-	currentHealth -= 1
-	update_health_display()
-	print("[GUESS] Wrong guesses:", wrong_guesses, " / ", MAX_GUESSES)
-	print("[GUESS] Current Health:", currentHealth, " / ", MAX_GUESSES)
-	if wrong_guesses >= MAX_GUESSES:
-		show_end("YOU LOSE!")
-
+			return
+	else:
+		wrong_guesses += 1
+		currentHealth -= 1
+		update_health_display()
+		print("[GUESS] Wrong guesses:", wrong_guesses, " / ", MAX_GUESSES)
+		print("[GUESS] Current Health:", currentHealth, " / ", MAX_GUESSES)
+		if wrong_guesses >= MAX_GUESSES:
+			show_end("YOU LOSE!")
+		
+	
 
 # ----------------------------------
 # UNDO
@@ -252,7 +303,7 @@ func undo():
 	update_ui()
 	print("[UNDO] restored state; undo stack size =", undo_stack.size)
 	print("[SAVE] saved state; redo stack size =", redo_stack.size)
-
+	$AudioStreamPlayer.play()
 # ----------------------------------
 # REDO
 # ----------------------------------
@@ -285,7 +336,7 @@ func redo():
 	update_ui()
 	update_health_display()
 	print("[REDO] restored state; redo stack size =", redo_stack.size)
-
+	$AudioStreamPlayer.play()
 # ----------------------------------
 # SHOW LOSE/WIN
 # ----------------------------------
@@ -338,7 +389,37 @@ func _on_continue_pressed():
 	# Clear result so it doesn't apply twice
 	last_round_result = ""
 
+# ----------------------------------
+# Dialogue
+# ----------------------------------
 
+func play_Day_Dialogue():
+	var index = current_day - 1
+	
+	if is_instance_valid(clue_button):
+		clue_button.visible = false
+	
+	if index < scene_dialogues.size():
+		var lines_for_the_day = scene_dialogues[index]
+		
+		await DialogueManager.line_player(lines_for_the_day, this_panel, this_label)
+	
+	#if is_instance_valid(this_panel):
+		#this_panel.visible = true
+		#this_label.text = " "
+	#
+	if is_instance_valid(clue_button):
+		clue_button.visible = true
+	
+
+func riddle_for_the_Day():
+	var index = current_day - 1
+	
+	if index < riddles.size():
+		var riddle_of_the_day = riddles[index]
+		
+		DialogueManager.line_player(riddle_of_the_day, this_panel, this_label)
+		
 
 
 # ----------------------------------
@@ -347,3 +428,7 @@ func _on_continue_pressed():
 func _on_return_main_pressed():
 	await get_tree().create_timer(0.5).timeout #added delay for aesthetics ahh
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_clue_button_pressed() -> void:
+	riddle_for_the_Day()
