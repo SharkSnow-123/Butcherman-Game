699a534b40cc90907b27fa95fc395d9eb4766a65
lauraescasinas <lauramargaret.escasinas@cit.edu>
2025-12-10 10:10:56 +0800

    dialogue bug fixed

--- Global/dialogue_manager2.gd
+++ Global/dialogue_manager2.gd
@@ -3,9 +3,9 @@ extends Node
 const TYPE_SPEED = 0.05
 const SHOWTIME = 2.0
 
-var type_timer = Timer
-var active_label = Label
-var active_panel = Panel
+var type_timer: Timer # changed
+var active_label: Label = null# changed
+var active_panel: Panel = null # changed
 var current_text: String = ""
 var charIndex: int = 0
 var is_typing: bool = false
@@ -23,14 +23,22 @@ func  line_player(lines: Array, target_panel: Panel, target_label: Label):
 	active_panel = target_panel
 	active_label = target_label
 	
+	if not is_instance_valid(active_panel) or not is_instance_valid(active_label): # added
+		return
+		
 	active_panel.visible = true
 	
 	for  line in lines:
 		await singleLine(line)
 	
-	active_panel.visible = false
+	if is_instance_valid(active_panel):
+		active_panel.visible = false
 
 func singleLine(text: String):
+	
+	if not is_instance_valid(active_label): # added
+		is_typing = false
+		return  
 	
 	active_label.text = ""
 	current_text = text
@@ -39,17 +47,26 @@ func singleLine(text: String):
 	
 	type_timer.start()
 	
-	while is_typing:
+	while is_typing: # fixed
 		await get_tree().process_frame
 	
 	await get_tree().create_timer(SHOWTIME).timeout
 
 func typeWriter():
 	
-	if active_label and charIndex < current_text.length():
+	if not is_instance_valid(active_label):
+		type_timer.stop()
+		is_typing = false
+		return
+	
+	if charIndex < current_text.length(): # fixed
 		active_label.text += current_text[charIndex]
 		charIndex += 1
 	else:
 		type_timer.stop()
 		is_typing = false
 	
+func stop_dialogue(): # new func
+	is_typing = false
+	if type_timer:
+		type_timer.stop()
