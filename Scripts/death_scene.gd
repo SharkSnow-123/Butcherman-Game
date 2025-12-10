b861396ee368ea2ea5711ccdd94a69a5152fdc59
lauraescasinas <lauramargaret.escasinas@cit.edu>
2025-12-10 10:13:04 +0800

    bug fixed

--- Scripts/death_scene.gd
+++ Scripts/death_scene.gd
@@ -4,5 +4,5 @@ func _on_losttomain_pressed() -> void:
 	TransitionScreen.fade_out("res://Scenes/main.tscn")
 	
 func _ready() -> void:
-	MusicPlayer.play_music("res://Music/death_scene_bgm.mp3")
+	MusicPlayer.play_music("res://Music/Eddie Gluskin-Chase theme.mp3")
 	
--- Scripts/end_scene_1.gd
+++ Scripts/end_scene_1.gd
@@ -4,4 +4,8 @@ func _on_wontomain_pressed() -> void:
 	TransitionScreen.fade_out("res://Scenes/main.tscn")
 
 func _ready() -> void:
-	MusicPlayer.play_music("res://Music/end_scene_bgm.mp3")
+	MusicPlayer.play_music("res://Music/Suspense Pad - Fourpin Sound - SoundLoadMate.com.mp3")
+
+
+func _on_wontomain_1_pressed() -> void:
+	TransitionScreen.fade_out("res://Scenes/main.tscn")
